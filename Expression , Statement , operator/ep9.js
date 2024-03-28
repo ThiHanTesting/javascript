c688d873359abc74a2376fff1b7e4d8033c3cac6
thi han <thihan245@gmail.com>
2024-03-28 18:47:38 +0630

    ep 10

---
+++ Expression , Statement , operator/ep9.js
@@ -0,0 +1,24 @@
+// other operators and operator percedence
+
+ // power 
+let z = 2**3  //2*2*2
+console.log(z)
+
+let a = 3
+a += 2  // a = a + 2
+console.log(a) // 5
+
+a -= 4  // a = a - 5
+console.log(a) // 1
+
+a *= 8 // a = a * 8
+console.log(a) // 8
+
+a /= 2 // a = a / 2
+console.log(a) // 4
+
+a %= 2 // a = a % 2
+console.log(a) // 0
+
+
+
