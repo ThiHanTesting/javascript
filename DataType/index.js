f7383a5244f909289c0260422e7ef3e9b9c7f276
thi han <thihan245@gmail.com>
2024-03-28 18:02:16 +0630

    ep4

---
+++ DataType/index.js
@@ -0,0 +1,10 @@
+// the way of writing string
+let greet = 'hello world'
+console.log(greet)
+
+let myname = "khun thi han "
+console.log(myname)
+
+// u can also write with this ``
+let greeting = `hi this is ${myname}`
+console.log(greeting)
