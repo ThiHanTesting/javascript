b0518f2b8f7c7dd24bc3a1367ada6b507cfd4855
thi han <thihan245@gmail.com>
2024-03-28 18:37:45 +0630

    ep8

---
+++ DataType/Expression , Statement , operator/ep8.js
@@ -0,0 +1,37 @@
+// arithmetic operators
+
+
+// sum is simple
+let z = "addf"+2
+let zz = 'asdf'+null
+let zzz = 'asfd'+undefined
+
+console.log(z)
+console.log(zz)
+console.log(zzz)
+
+// multiple
+let x = 2*3
+let xx = '2'*3
+let xxx = 'ad'*3
+
+
+console.log(x)
+console.log(xx)
+console.log(xxx)
+
+// divide (1) /  (2) %
+let y = 3/2
+let yy = "3"/2
+let yyy = 'ad'/2
+
+
+console.log(y)
+console.log(yy)
+console.log(yyy)
+
+
+
+// assignment ooperator
+
+
---
+++ DataType/Expression , Statement , operator/ep8Assignment.js
@@ -0,0 +1,3 @@
+let a,b,c = 0
+console.log(a)
+console.log(c)
