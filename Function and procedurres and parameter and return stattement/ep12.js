78f99ba00ab7758616baca86f480c6fa23b9c7bb
thi han <thihan245@gmail.com>
2024-03-28 19:54:52 +0630

    tp12

---
+++ Function and procedurres and parameter and return stattement/ep12.js
@@ -0,0 +1,19 @@
+// functions 
+
+function shopping(product){
+    console.log("start to go shopping")
+    console.log('going to buy '+product)
+    console.log('go back to home')
+} 
+
+shopping('banana')
+shopping('apple') 
+
+
+
+function add( num1 , num2 ){
+    return num1 + num2 ;
+}
+console.log(add(2,3))
+let result = add(5,7)
+console.log(result)
