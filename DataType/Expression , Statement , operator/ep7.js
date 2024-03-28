86cedf0ebddc4d21c866431e23ac13044377a63e
thi han <thihan245@gmail.com>
2024-03-28 18:29:45 +0630

    ep 7

---
+++ DataType/Expression , Statement , operator/ep7.js
@@ -0,0 +1,36 @@
+// go and test in the browser console
+
+// expression always return a value
+// 5+1
+// "a"+"b"
+// 5-4
+
+
+//statement
+let sum = 1+2 //statement ==> undefined
+
+// operator
+// 1 . arithmetic
+// 2 . assignment
+// 3 . comparison
+// 4 . logical
+
+
+let z = 'a'+'b'
+let xx = 3 + 2
+let xxx = 3 + "b"
+let xxxx = "b" + 3
+let zzz = "2"-"1"   // NUMBER
+let zzzz = "a"-"b" // NaN
+let y = "3"-2 // number
+let yy = 'a'-2 // NaN
+
+console.log(z)
+console.log(xx)
+console.log(xxx)
+console.log(xxxx)
+console.log(zzz)
+console.log(zzzz)
+console.log(y)
+console.log(yy)
+
