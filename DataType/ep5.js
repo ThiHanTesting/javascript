0745d731d620c6e4513ebfaa5727afd562df05db
thi han <thihan245@gmail.com>
2024-03-28 18:13:19 +0630

    ep5

---
+++ DataType/ep5.js
@@ -0,0 +1,17 @@
+let myVar = true ;
+console.log(myVar) ;
+
+
+let myname  ='khun thih na'
+Boolean(myname) 
+
+
+// falsy values
+// 1 0
+// 2 ""
+// 3 null 
+// 4 undefined
+// 5 NaN
+// 6 false
+
+console.log(Boolean("")) ;
