2d9eacca9401df6edf612361a53f98b84c414649
thi han <thihan245@gmail.com>
2024-03-28 18:59:10 +0630

    ep10

---
+++ Expression , Statement , operator/ep10.js
@@ -0,0 +1,24 @@
+// comparsion operator ( > , < , >= , <= , == , === , != , !== )
+6 > 5 // true
+6 < 5 // false
+4<3 // true
+
+5 >= 5 // true
+1<=3 // true
+
+// == 
+2==2 // true
+2=='2'//ture
+
+// === (strict equal )
+2===2 // true
+2==='2' // false
+
+// !=
+2!=2 // false   
+2!='2' // false
+
+// !===
+2!==2 // false
+2!=='2' // true
+
