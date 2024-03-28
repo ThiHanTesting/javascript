b7f72046d485b4bf249536dfb61b85ff7ae14958
thi han <thihan245@gmail.com>
2024-03-28 18:19:02 +0630

    ep6

---
+++ DataType/ep6.js
@@ -0,0 +1,18 @@
+// undefined
+let x ;
+console.log(x) ; // undefined
+
+// null
+let y = null 
+console.log(y) ; // null
+
+// not a number (NaN)
+let num1 ;
+let num2 = 2 
+console.log(num1+num2) // NaN
+
+
+
+console.log(Boolean(x)) ;
+console.log(Boolean(y)) ;
+console.log(Boolean(num1+num2)) ;
