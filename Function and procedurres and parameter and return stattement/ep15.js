877a9b4d59c82c92034e29b2c9f2237e89db5ce7
thi han <thihan245@gmail.com>
2024-03-29 16:14:10 +0630

    ep15

---
+++ Function and procedurres and parameter and return stattement/ep15.js
@@ -0,0 +1,15 @@
+// function statement 
+function myName(){
+    console.log('my name is <NAME>')
+}
+
+
+//function expression  let a = 1 + 2
+let sayHi = function(e){
+    console.log(e)
+}
+
+
+
+sayHi('hi there')
+myName()
