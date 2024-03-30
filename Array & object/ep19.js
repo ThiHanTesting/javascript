856cfd5462398281eeb03180c5f4c310e155abf8
thi han <thihan245@gmail.com>
2024-03-30 12:44:47 +0630

    real ep20

--- Array & object/ep19.js
+++ Array & object/ep19.js
@@ -1,4 +1,20 @@
 // array
 let mixture2 = new Array('dog' , 20 , 1.5 , true) // old style
 let mixture = ['dog' , 20 , 1.5 , true]
-console.log(mixture.length)
+
+mixture[4] = 'cat'
+mixture[3] = false
+console.log(mixture)
+
+
+let fruits = ['apple' , 'orange' ]
+fruits[2] =  'banana'
+console.log(fruits[fruits.length-1])
+
+
+// nested array
+let arr = [
+    ['a','b','c','d'],
+    [1,2,3]
+]
+console.log(arr[0][3])
