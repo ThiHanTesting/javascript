127031058b8be1d67de722d50abbcdabcee07dd0
thi han <thihan245@gmail.com>
2024-03-30 12:19:33 +0630

    ep18

---
+++ Function and procedurres and parameter and return stattement/ep18.js
@@ -0,0 +1,22 @@
+//arrow function
+
+let sayHi = ()=> console.log('hi')  // first advantage
+
+let sayHelloWorld = ()=> 'hello world'; // second advantage
+let sum = ()=> 9+2;
+let sayWhat = (word)=> word ;
+
+let sayhello = word => 'hello'+word ; // third advantage 
+
+
+
+// sayHi()
+// console.log(sayHelloWorld())
+// console.log(sum())
+// console.log(sayWhat('what'))
+// console.log(sayhello('hello'))
+
+
+let twice = num => num*2 
+console.log(twice(5))   
+
