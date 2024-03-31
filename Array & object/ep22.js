93faccf86c7d24b065cdc0a77235bd385d3321fd
thi han <thihan245@gmail.com>
2024-03-31 09:31:20 +0630

    ep23

---
+++ Array & object/ep22.js
@@ -0,0 +1,16 @@
+// array methods
+
+let animals = ['dog','cat','tiger']
+
+
+// animals.push('bird')
+// animals.pop()
+// animals.shift()
+// animals.unshift('elephant')
+
+console.log(animals.indexOf('cat'))  // return int
+console.log(animals.join('-'))    // return string
+animals.splice(0,2)  // from 0 and delete 2 
+
+console.log(animals)
+
