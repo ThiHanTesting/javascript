72e916ae65e9dbbfe71e2e09f269f08677d697c3
thi han <thihan245@gmail.com>
2024-03-31 09:44:13 +0630

    real ep23 map method

---
+++ Array & object/ep23.js
@@ -0,0 +1,20 @@
+// arrays methods
+
+let nums = [1,2,3,4,5,6,7,8,9]
+console.log(nums)
+nums = nums.map((num)=>{
+    return num+100
+})
+
+console.log(nums)
+
+
+
+let names = ['james','john','mary']
+console.log(names)
+names = names.map((name)=>{
+    return "Mrs."+name
+})
+
+
+
