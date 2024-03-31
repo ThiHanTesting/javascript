c63ff1b50f446d49da2b209259699d784965875f
thi han <thihan245@gmail.com>
2024-03-31 11:04:33 +0630

    real ep27

---
+++ Array & object/ep27.js
@@ -0,0 +1,12 @@
+// let str = 'hello'
+let str = new String(' hello' )
+// console.log(typeof str)
+// console.log(str)
+// console.log(str.length)
+// console.log(str.indexOf('o'))
+// str = str.trim()
+
+let str2 = "hello thi han"
+let newArr = str2.split(' ')
+console.log(newArr)
+
